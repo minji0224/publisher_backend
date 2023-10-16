88a5a059f9262ce3bfcb86a535e59fd8bae3727b
Minji Choi <minj22222c@gmail.com>
2023-10-16 12:42:59 +0900

    first commit

---
+++ publisher/src/test/kotlin/com/cmj/publisher/PublisherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cmj.publisher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
