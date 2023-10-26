4a3b53ff5cc0d5332a4dd8900bb5cdbe3b6ee231
Minji Choi <minj22222c@gmail.com>
2023-10-26 16:55:17 +0900

    검색 진행중

--- publisher/src/main/kotlin/com/cmj/publisher/book/BookController.kt
+++ publisher/src/main/kotlin/com/cmj/publisher/book/BookController.kt
@@ -5,10 +5,12 @@ import com.cmj.publisher.auth.AuthProfile
 
 import com.cmj.publisher.cummerce.RabbitProducer
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.jetbrains.exposed.sql.batchInsert
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -16,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
+import java.sql.Connection
 import java.time.LocalDateTime
 import java.util.*
 
@@ -142,8 +145,114 @@ class BookController(private val rabbitProducer: RabbitProducer) {
         return ResponseEntity.status(HttpStatus.CREATED).body(result)
     }
 
+
+    @GetMapping("/paging/search")
+    fun searchPaging(@RequestParam size: Int, @RequestParam page: Int,
+                     @RequestParam keyword: String?, @RequestParam option: String?) : Page<BookResponse>
+    = transaction(Connection.TRANSACTION_READ_UNCOMMITTED, readOnly = true) {
+
+
+        val query = when {
+            keyword != null -> Books.select {
+                Books.title like "%${keyword}"
+            }
+            else -> Books.selectAll()
+        }
+
+        val totalCount = query.count()
+        println("토탈카운트값: $totalCount")
+
+        val content = query.orderBy(Books.id to SortOrder.DESC).limit(size, offset = (size * page).toLong())
+            .map {r ->
+                BookResponse(
+                    r[Books.id],
+                    r[Books.publisher],
+                    r[Books.title],
+                    r[Books.author],
+                    r[Books.pubDate],
+                    r[Books.isbn],
+                    r[Books.categoryName],
+                    r[Books.priceStandard].toString(),
+                    r[Books.quantity].toString(),
+                    r[Books.createdDate].toString()
+                )
+            }
+
+        println(size)
+        println(page)
+        println(keyword)
+        println(option)
+        PageImpl(content, PageRequest.of(page, size), totalCount)
+    }
+
+
+    data class SearchRequest(
+            val keyword: String?,
+            val option: String?,
+            val createdDate: String?,
+            val page: Int,
+            val size: Int
+    )
+
+    @PostMapping("/test")
+    fun searchItem(@RequestBody searchRequest: SearchRequest) : Page<BookResponse>
+    = transaction(Connection.TRANSACTION_READ_UNCOMMITTED, readOnly = true) {
+
+        println(searchRequest)
+
+        val query = when {
+            searchRequest.keyword != null && searchRequest.option != null -> {
+                when(searchRequest.option) {
+                    "title" -> Books.select { Books.title like "%${searchRequest.keyword}" }
+                    "author"-> Books.select { Books.author like "%${searchRequest.keyword}" }
+                    "isbn" -> Books.select { Books.isbn like "%${searchRequest.keyword}" }
+                    else -> error("잘못된 옵션값 : Invalid option")
+                }
+            }
+            searchRequest.keyword != null -> {
+                Books.select {
+                    (Books.title like "%${searchRequest.keyword}") or
+                    (Books.author like "%${searchRequest.keyword}") or
+                    (Books.isbn like "%${searchRequest.keyword}")
+                }
+            }
+            else -> Books.selectAll()
+        }
+
+
+        val totalCount = query.count()
+        println("토탈카운트: $totalCount")
+
+        val content = query.orderBy(Books.id to SortOrder.DESC).limit(searchRequest.size,
+                offset = (searchRequest.size * searchRequest.page).toLong())
+                .map {r ->
+                    BookResponse(
+                            r[Books.id],
+                            r[Books.publisher],
+                            r[Books.title],
+                            r[Books.author],
+                            r[Books.pubDate],
+                            r[Books.isbn],
+                            r[Books.categoryName],
+                            r[Books.priceStandard].toString(),
+                            r[Books.quantity].toString(),
+                            r[Books.createdDate].toString()
+                    )
+                }
+
+        println("셀렉트한 컨텐트: $content")
+        PageImpl(content, PageRequest.of(searchRequest.page, searchRequest.size), totalCount)
+
+
+}
+
+
+
+
+
+
+
+
     // 파일다운메서드 만들기
-    // 페이지만들기
-    // 서치만들기
 
 }
